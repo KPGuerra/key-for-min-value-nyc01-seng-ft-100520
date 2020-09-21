bd58c77b0e76f6b57e94897b4e3e994334358fc9
Karina Guerra <kpguerra96@gmail.com>
2020-09-21 21:46:21 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,6 +8,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if smallest_value == 0
       smallest_value = value
+      smallest_value_key = key
       
     elsif value < smallest_value
       smallest_value = value
